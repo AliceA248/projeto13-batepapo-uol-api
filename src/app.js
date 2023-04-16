051eb3a884d50f10cb3474ae139f079b4cc0311f
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-04-16 19:57:49 -0300

    Adicionar participantes, corrigindo erros

--- src/app.js
+++ src/app.js
@@ -1,91 +1,131 @@
-import express from "express"
-import cors from "cors"
-import { MongoClient } from "mongodb"
-import joi from "joi"
-import dotenv from "dotenv"
-import dayjs from "dayjs"
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import joi from "joi";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
 
-dotenv.config()
+dotenv.config();
+
+const messageValidacaoTipo = joi.object({
+  from: joi.string().required(),
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().required().valid("message", "private_message"),
+});
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 let db;
 
 try {
-  await mongoClient.connect()
-  db = mongoClient.db()
+  await mongoClient.connect();
+  db = mongoClient.db();
 } catch (error) {
-    console.log(error)
-    console.log("Não está rodando corretamente")
+  console.log(error);
+  console.log("Não está rodando corretamente");
 }
 
-const participantsValidação =joi.object({
-    name: joi.string().required().trim()
-})
+const participantsValidacao = joi.object({
+  name: joi.string().trim().required(),
+});
 
 // Criação do servidor
-const app = express()
+const app = express();
 
 // Configurações
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // Rotas
 
 app.get("/participants", async (req, res) => {
-    try {
-        const todosParticipantes = await db.collection("participants").find().toArray()
-        res.send(todosParticipantes)
-    } catch (error) {
-        console.error(error)
-        res.status(500).send("O banco não está rodando corretamente")
+  try {
+    const todosParticipantes = await db.collection("participants").find().toArray();
+
+    if (!todosParticipantes.length) {
+      return res.status(404).send("Nenhum usuário foi encontrado");
     }
-})
+
+    res.send(todosParticipantes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("O banco não está rodando corretamente");
+  }
+});
 
 app.post("/participants", async (req, res) => {
-    const { name } = req.body
+  const { name } = req.body;
 
-    const { error } = participantsValidação.validate({ name })
+  const { error } = participantsValidacao.validate({ name });
 
-    if (error) return res.status(422).send("")
+  if (error) {
+    return res.status(422).send(error.details.map((err) => err.message));
+  }
 
-    try {
-        const nameCheck = await db.collection("participants").findOne({ name: name})
+  try {
+    const nameCheck = await db.collection("participants").findOne({ name });
 
-        if (nameCheck) return res.status(409).send("O usuário já existe")
-
-        await db.collection("participants").insertOne( { name: name, lastStatus: Date.now()}) 
-
-        
-        /* 
-         {
-            from: 'xxx',
-            to: 'Todos',
-            text: 'entra na sala...',
-            type: 'status',
-            time: 'HH:mm:ss'
-        }
-         */
-
-        await db.collection("messages").insertOne({
-            from: name,
-            to: "Todos",
-            text: "Entra na sala....",
-            type: "status",
-            time: dayjs(Date.now()).format("HH:mm:ss")
-
-        }) 
-
-        res.status(201).send("Tudo certo!")
-        
-    } catch (error) {
-        res.status(500).send("Servidor não está rodando corretamente")
+    if (nameCheck) {
+      return res.status(409).send("O usuário já existe");
     }
 
+    await db.collection("participants").insertOne({
+      name,
+      lastStatus: Date.now(),
+    });
 
-} )
+    await db.collection("messages").insertOne({
+      from: name,
+      to: "Todos",
+      text: "Entra na sala....",
+      type: "status",
+      time: dayjs(Date.now()).format("HH:mm:ss"),
+    });
+
+    res.status(201).send("Tudo certo!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Servidor não está rodando corretamente");
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+
+  const novaMensagem = {
+    to,
+    text,
+    type,
+    from: user,
+  };
+
+  const { error } = messageValidacaoTipo.validate(
+    { to, text, type, from: user },
+    { abortEarly: false }
+  );
+
+  if (error) {
+    const erroMensagem = error.details.map((e) => e.message);
+    return res.status(422).send(erroMensagem);
+  }
+
+  const participanteCadastrado = await db.collection("participants").findOne({ name: user })
+
+  if (!participanteCadastrado) return res.status(422).send 
 
 
+
+
+  await db.collection("messages").insertOne({
+    ...novaMensagem,
+    time: dayjs().format("HH:mm:ss"),
+  });
+
+  res.status(201).send("Tudo rodando corretamente");
+});
 
 // Deixa o app escutando, à espera de requisições
-const PORT = 5001
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+const PORT = 5001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+  
